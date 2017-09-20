d5e1759776881438344f0927b7b837ee9f701ecd
Shadi Tasdighi Kalat <shaditasdighikalat@Shadis-MacBook-Air.local>
2017-09-20 17:59:26 -0400

    i did nothing

---
+++ build/CMakeFiles/nest-defusal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nest-defusal.dir/defuse.c.o"
+  "nest-defusal.pdb"
+  "nest-defusal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nest-defusal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
